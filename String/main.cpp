ba3826c63ec8362a2c1dbfeeff5b80bd4cf40ce2
xjhahah <908234867@qq.com>
2018-11-12 23:50:19 +0800

    提交代码，修复已知bug

--- String/main.cpp
+++ String/main.cpp
@@ -4,12 +4,13 @@ int main()
 {
 	String s1("hello");
 	cout << s1 << endl;
+	/*
 	s1.PushBack(' ');
 	cout << s1.Find('e', 0) << endl;
 	s1.Find("ell", 0);
 	cout << s1.Find("ell", 0) << endl;
-	s1.Insert(2, 'p');
-	s1.Insert(5, "world");
-	s1.Erase(1, 2);
+	s1.Insert(2, 'p');*/
+	s1.Insert(5, " world");
+	//s1.Erase(1, 2);
 	return 0;
 }
